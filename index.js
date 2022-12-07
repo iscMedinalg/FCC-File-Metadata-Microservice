393a13a3d07327bb604a8bcb8a1eba9cef5e9439
GustavoML <iscmedinalg@gmail.com>
2022-12-07 12:16:28 -0600

    Test Finished

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const upload = multer({dest:'./public/data/uploads'});
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +13,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size} = req.file
+  req.file.originalname
+  req.file.mimetype
+  req.file.size
+  res.json({
+    "name": originalname,
+    "type": mimetype, 
+    "size": size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
